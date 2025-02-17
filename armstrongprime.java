d0ee2e802c7238b1dc9723035ea7268951903a2a
rajavarapu <srisaivardhan022@gmail.com>
2025-02-17 12:41:17 +0530

    commit 1

---
+++ armstrongprime.java
@@ -0,0 +1,37 @@
+class Name
+{
+ 	public static void main(String[] args)
+	{
+		int num=153,power=0,count=0,temp=num,sum=0,start=1;
+		for(;num!=0;num=num/10)
+		{
+			int rem=num%10;
+			count++; 
+		
+		}     
+		System.out.println(count );                  
+		num=temp;
+		for(;num!=0;num/=10)
+		{
+			int rem=num%10;
+			power=(int)Math.pow(rem,count);
+				sum+=power;
+		}
+		System.out.println(sum);
+		num=temp;
+		if(sum==temp)
+		{=
+			for(;start<=num;start++)
+			{
+				if(num%start==0)
+					count++;
+			}
+			if(count==2)
+				System.out.println(temp+" arm strong prime");
+			else
+				System.out.println(temp+" not an armstrong prime");
+		}
+		else
+			System.out.println(temp+" not an armstrong prime");
+	}
+}
---
+++ extraction.java
@@ -0,0 +1,22 @@
+class Myname
+{
+public static void main(String[] args)
+	{
+	int num=1437,rem=0,count=0;
+	        while(num!=0)	
+		{
+		rem=num%10;
+		count++;
+		num=num/10;
+		System.out.println(rem);
+        
+		}
+		System.out.println(count);
+
+        }
+
+
+
+	
+}
+		
